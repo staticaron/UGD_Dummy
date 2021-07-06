94eae8b5b8a42f6640e20f26320c970f53af10fe
Devanshu Mishra <devm19102003@gmail.com>
2021-07-06 16:08:55 +0530

    Added Ball Movement on click

---
+++ Assets/GameFiles/Enums/BallMoveState.cs
@@ -0,0 +1,5 @@
+public enum BallMoveState
+{
+    MOVING,
+    READY
+}
---
+++ Assets/GameFiles/Scripts/Launcer.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+
+public class Launcer : MonoBehaviour
+{
+    #region Properties
+    [SerializeField, Space] Transform ball;
+
+    [SerializeField] AnimationCurve ballMovementCurve;
+    [SerializeField] float ballSpeedModifier = 1;
+
+    [SerializeField, Space] Transform rayPoint;
+    [SerializeField] LayerMask rayMask;
+
+    [SerializeField, Space] BallMoveState currentBallMoveState = BallMoveState.READY;
+
+    [SerializeField] RaycastHit rayHitData;
+    [SerializeField] Vector3 initialPos;
+    [SerializeField] float slider = 0f;
+    #endregion
+
+    #region MonobehavioursFunctions
+    private void Start()
+    {
+        initialPos = ball.position;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            LaunchBall();
+        }
+
+        if (currentBallMoveState == BallMoveState.MOVING)
+        {
+            slider += Time.deltaTime * ballSpeedModifier;
+            slider = Mathf.Clamp(slider, 0, 1);
+
+            float yVal = ballMovementCurve.Evaluate(slider) * rayHitData.distance;
+
+            ball.position = new Vector3(initialPos.x, initialPos.y - yVal, initialPos.z);
+
+            if (slider == 1)
+            {
+                currentBallMoveState = BallMoveState.READY;
+                slider = 0;
+            }
+        }
+    }
+    #endregion
+
+    #region Private Functions
+    private void LaunchBall()
+    {
+        Physics.Raycast(rayPoint.position, rayPoint.forward, out rayHitData, Mathf.Infinity, rayMask);
+
+        Debug.Log(rayHitData.distance);
+
+        currentBallMoveState = BallMoveState.MOVING;
+    }
+    #endregion
+
+    #region Public Functions
+
+    #endregion
+}
